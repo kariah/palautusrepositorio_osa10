f3da4b7c2accd71168cf0dd9716206223bc171e6
kariah <41475006+kariah@users.noreply.github.com>
2021-08-18 15:22:30 +0300

    config changes

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
-export default function(api) {
+// eslint-disable-next-line no-undef
+module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
   };
-}
+};
