433dd429ab9a0348c0002f8466073dbe7e0d4957
Kari Heiskanen <kari.a.heiskanen@gmail.com>
2021-09-18 14:17:05 +0300

    small fixes

--- src/components/Repository/RepositoryList.jsx
+++ src/components/Repository/RepositoryList.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import { useState } from 'react';
 import useRepositories from "../../hooks/useRepositories";
 import RepositoryListContainer from "./RepositoryListContainer";
-
-/*<View style={{ zIndex: 0 }} ></View>*/
+ 
 const RepositoryList = () => {
     const [sorting, setSorting] = useState({
         orderBy: "CREATED_AT",
